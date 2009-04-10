c184bc0af225a99a3c1aca7abda6201b0e1a396e
Jon M. Dugan <jdugan@x1024.net>
2009-04-10 23:50:41 +0000

    implement simple IP address based acls

--- src/python/esxsnmp/ipaclsocket.py
+++ src/python/esxsnmp/ipaclsocket.py
@@ -1,13 +1,26 @@
+import socket 
 from thrift.transport.TSocket import TSocket, TServerSocket
+from esxsnmp.util import get_logger
 
 class IPACLSocket(TServerSocket):
-    def __init__(self, port, permit):
+    def _resolveAddr(self):
+        return socket.getaddrinfo(self.host, self.port, socket.AF_INET,
+                socket.SOCK_STREAM, 0, socket.AI_PASSIVE |
+                socket.AI_ADDRCONFIG)
+    def __init__(self, port, permit, host='0.0.0.0', log=None):
         self.permit = permit
+        self.host = host
+        self.port = port
+        self.log = log
         TServerSocket.__init__(self, port)
 
     def accept(self):
         (client, addr) = self.handle.accept()
-        print client, addr
+        if not addr[0].startswith('198.129'):
+            if self.log:
+                self.log.info('connection rejected from %s' % addr[0])
+            client.close()
+            return None
         result = TSocket()
         result.setHandle(client)
         return result
