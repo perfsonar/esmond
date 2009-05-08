f43fc521a82ca7a4d2901ef1f627d0414af355fc
Jon M. Dugan <jdugan@x1024.net>
2009-05-08 21:38:40 +0000

    add tests (woot!)

---
+++ tests/python/test_correlator.py
@@ -0,0 +1,54 @@
+from esxsnmp.poll import IfDescrCorrelator, JnxFirewallCorrelator, JnxCOSCorrelator
+
+class MockSession(object):
+    def walk(self, oid):
+        if oid == 'ifDescr':
+            return (('ifDescr.115', 'ae0'),
+                    ('ifDescr.116', 'ge-1/0/0'))
+        elif oid == 'ifHCInOctets':
+            return (('ifHCInOctets.115', '0', 'ifHCInOctets/ae0'),
+                    ('ifHCInOctets.116', '732401229666', 'ifHCInOctets/ge-1_0_0'))
+        elif oid == 'jnxCosIfqQedBytes':
+            return (('jnxCosIfqQedBytes.116."best-effort"', '2091263919975',
+                        "ge-1_0_0/jnxCosIfqQedBytes/best-effort"),
+                    ('jnxCosIfqQedBytes.116."network-control"', '325426106',
+                        "ge-1_0_0/jnxCosIfqQedBytes/network-control"),
+                    ('jnxCosIfqQedBytes.116."scavenger-service"', '17688108277',
+                        "ge-1_0_0/jnxCosIfqQedBytes/scavenger-service"),
+                    ('jnxCosIfqQedBytes.116."expedited-forwarding"', '1026807',
+                        "ge-1_0_0/jnxCosIfqQedBytes/expedited-forwarding"))
+        elif oid == 'jnxFWCounterByteCount':
+            return (('jnxFWCounterByteCount."fnal-test"."fnal".counter',
+                     '0', 'counter/fnal-test/fnal'),
+                    ('jnxFWCounterByteCount."fnal-test"."discard".counter',
+                     '0', 'counter/fnal-test/discard'),
+                    ('jnxFWCounterByteCount."test-from-eqx"."from-eqx".counter',
+                     '0', 'counter/test-from-eqx/from-eqx'))
+
+class MockOID(object):
+    def __repr__(self):
+        return "MockOID('%s')" % self.name
+
+    def __init__(self, name):
+        self.name = name
+
+def check_correlator(correlator, oid):
+    s = MockSession()
+    c = correlator(s)
+    c.setup()
+    for (var,val,check) in s.walk(oid.name):
+        assert check == c.lookup(oid, var)
+
+def test_correlators():
+    for (correlator, oid) in ((IfDescrCorrelator, MockOID('ifHCInOctets')),
+            (JnxFirewallCorrelator, MockOID('jnxFWCounterByteCount')),
+            (JnxCOSCorrelator, MockOID('jnxCosIfqQedBytes'))):
+        yield check_correlator, correlator, oid
+
+#def test_jnx_cos_correlator():
+#    s = MockSession()
+#    c = JnxCOSCorrelator(s)
+#    c.setup()
+#    for (var,val,check) in s.walk('jnxCosIfqQedBytes'):
+#        assert check == c.lookup('jnxCosIfqQedBytes', var)
+
