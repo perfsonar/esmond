437e20dcdfb13aacc5baea04c35e167a0c4a2bbd
Jon M. Dugan <jdugan@x1024.net>
2012-10-24 23:48:24 -0600

    add command to list devices and oidsets

---
+++ esxsnmp/api/management/commands/list.py
@@ -0,0 +1,16 @@
+import sys
+import datetime
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+from esxsnmp.api.models import Device
+
+class Command(BaseCommand):
+    args = ''
+    help = 'List Devices and associated OIDSets'
+
+    def handle(self, *args, **options):
+        for device in Device.objects.active():
+            oidsets = [o.name for o in device.oidsets.all()]
+            print device.name + ": " + ", ".join(oidsets)
