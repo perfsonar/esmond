3cf316501c84a0e15cf2f133b886138b12f846ed
Monte Goode <MMGoode@lbl.gov>
2013-01-23 14:53:00 -0800

    Removed sqlite dep since it always breaks and python should have it, added pycassa lib.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='esxsnmp',
         author_email='jdugan@es.net',
         url='http://code.google.com/p/esxsnmp/',
         packages=['esxsnmp', 'esxsnmp.api', 'esxsnmp.admin'],
-        install_requires=['tsdb', 'Django==1.4.1', 'django-tastypie', 'web.py', 'simplejson', 'python-memcached', 'pymongo', 'pysqlite', 'mongoctl'],
+        install_requires=['tsdb', 'Django==1.4.1', 'django-tastypie', 'web.py', 'simplejson', 'python-memcached', 'pymongo', 'pycassa', 'mongoctl'],
         entry_points = {
             'console_scripts': [
                 'espolld = esxsnmp.poll:espolld',
