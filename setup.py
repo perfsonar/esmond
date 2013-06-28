14fea575c6a31b10359c6f160e07309c89ccae7e
Jon M. Dugan <jdugan@x1024.net>
2013-06-27 20:59:53 -0500

    upgrade to Django 1.5.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='esmond',
         author_email='jdugan@es.net',
         url='http://code.google.com/p/esmond/',
         packages=['esmond', 'esmond.api', 'esmond.admin'],
-        install_requires=['tsdb', 'Django==1.4.1', 'django-tastypie', 'web.py',
+        install_requires=['tsdb', 'Django==1.5.1', 'django-tastypie', 'web.py',
             'python-memcached', 'pycassa', 'psycopg2'],
         entry_points = {
             'console_scripts': [
