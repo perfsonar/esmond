e27006230a5164d8c96b05a8dcaa877fcb11ecde
Jon M. Dugan <jdugan@x1024.net>
2013-06-28 12:47:26 -0500

    add test to ensure that we can't post data

--- esmond/api/tests/api.py
+++ esmond/api/tests/api.py
@@ -1,15 +1,20 @@
 import json
 
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from tastypie.test import ResourceTestCase
 
 from esmond.api.models import *
 
-class DeviceAPITests(TestCase):
+class DeviceAPITests(ResourceTestCase):
     def setUp(self):
-        rtr_a = Device.objects.get_or_create(
+        self.rtr_a = Device.objects.get_or_create(
                 name="rtr_a", 
                 community="public")
+
+        self.rtr_b_post_data = {
+            "name": "rtr_b",
+            "community": "private",
+        }
 
     def test_list(self):
         url = '/v1/device/'
@@ -19,4 +24,13 @@ class DeviceAPITests(TestCase):
 
         data = json.loads(response.content)
         self.assertEquals(len(data), 1)
+
+    def test_post_list_unauthenticated(self):
+        """We don't allow POSTs at this time.  Once that capability is added
+        these tests will need to be expanded."""
+
+        self.assertHttpMethodNotAllowed(
+                self.client.post('/v1/device/entries/', format='json',
+                    data=self.rtr_b_post_data))
+
 
