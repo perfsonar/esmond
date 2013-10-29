1bc553f1ccb9f8ae0eaecba07386a880de46b04a
Monte Goode <MMGoode@lbl.gov>
2013-10-29 08:24:17 -0700

    New command script to add device/oidset change permissions to db for API key auth.  The perms being set don't seem quite right yet, but it's a start.

---
+++ esmond/api/management/commands/add_device_put_user.py
@@ -0,0 +1,58 @@
+import sys
+import datetime
+
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User, Group, Permission
+
+from tastypie.models import ApiKey
+
+from esmond.api.models import Device, OIDSet, DeviceOIDSetMap
+
+class Command(BaseCommand):
+    args = 'username'
+    help = 'Add a user for POST access'
+
+    def handle(self, *args, **options):
+        self.options = options
+
+        if len(args) < 1 or len(args) > 1:
+            print >>sys.stderr, "takes one argument: %s" % self.args
+            return
+
+        user = args[0]
+
+        u = None
+
+        try:
+            u = User.objects.get(username=user)
+            print 'User {0} exists'.format(user)
+        except User.DoesNotExist:
+            print 'User {0} does not exist - creating'.format(user)
+            u = User(username=user, is_staff=True)
+            u.save()
+
+        print 'Setting device PUT permissions.'
+        for model_name in ['device', 'deviceoidsetmap', 'devicetagmap']:
+            for perm_name in ['add', 'change', 'delete']:
+                perm = Permission.objects.get(codename='{0}_{1}'.format(perm_name, model_name))
+                print perm
+                u.user_permissions.add(perm)
+
+        u.save()
+            
+        try:
+            key = ApiKey.objects.get(user=u)
+            print 'User {0} already has api key, skipping creation'.format(user)
+        except ApiKey.DoesNotExist:
+            print 'User {0} does not have an api key - creating'.format(user)
+            u_apikey = ApiKey(user=u)
+            u_apikey.key = u_apikey.generate_key()
+            u_apikey.save()
+            u.save()
+
+        key = ApiKey.objects.get(user=u)
+
+        print 'Key: {0}'.format(key)
+
+        
