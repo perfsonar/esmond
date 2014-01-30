a11746a7ca32a9e5eb7e51742bd9437c84202de8
Andy Lake <andrewrlake@gmail.com>
2014-01-30 13:02:56 -0500

    Adding shell script to generate some bwctl and owamp data and moving fixture to same directory just so its easier to find. Will probably do massive reorganization of this after we get some real tests

---
+++ esmond/api/tests/perfsonar/generate_bwctl_owamp_data.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+#6 months of Bwctl data
+####10g Throughput results
+./ps_generate_data.py -k esmond -d base_rate -t 15552000 --key-prefix "ps:throughput" -m "EC7E5AF67F8746C8AEF41E60288F3F59" --minval 0 --maxval 10000000000 -w 14400
+
+#6 months of Owamp data
+####1 minute (base) summaries of one-way delay
+./ps_generate_data.py -k esmond -d histogram -t 15552000 --keep-data --key-prefix "ps:histogram_owdelay" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 0 --maxval 1000 --sample-size 600 -w 60
+
+#####1 day summaries of one-way delay
+./ps_generate_data.py -k esmond -d histogram -t 15552000 --keep-data --key-prefix "ps:histogram_owdelay" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 0 --maxval 1000 --sample-size 864000 -T aggregation -w 86400
+
+####1 minute (base) summaries of ttl
+./ps_generate_data.py -k esmond -d histogram -t 15552000 --keep-data --key-prefix "ps:histogram_ttl" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 9 --maxval 10 --sample-size 600 -w 60
+
+####1 minute (base) summaries of number of packets lost
+./ps_generate_data.py -k esmond -d base_rate -t 15552000 --keep-data --key-prefix "ps:packet_count_lost" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 0 --maxval 600 -w 60
+
+####1 minute (base) summaries of number of packets sent
+./ps_generate_data.py -k esmond -d base_rate -t 15552000 --keep-data --key-prefix "ps:packet_count_sent" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 0 --maxval 600 -w 60
+
+####1 minute (base) summaries of number of packet duplicates
+./ps_generate_data.py -k esmond -d base_rate -t 15552000 --keep-data --key-prefix "ps:packet_duplicates" -m "0CB19291FB6D40EAA1955376772BF5D2" --minval 0 --maxval 600 -w 60
