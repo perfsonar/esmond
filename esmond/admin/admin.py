7722b5f1172b77f915c8abe9cec95a9b8ef6efd5
Andy Lake <andy@es.net>
2015-02-11 15:08:04 -0500

    Adding User IPs to admin GUI

--- esmond/admin/admin.py
+++ esmond/admin/admin.py
@@ -33,4 +33,4 @@ admin.site.register(PSMetadata)
 admin.site.register(PSPointToPointSubject)
 admin.site.register(PSEventTypes)
 admin.site.register(PSMetadataParameters)
-
+admin.site.register(UserIpAddress)
