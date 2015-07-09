74b731ee95d4046ba42da2b0359c8cb21aa5d02d
Monte Goode <MMGoode@lbl.gov>
2015-07-09 10:25:26 -0700

    Basic main nested URI structure sanity checkin.

--- esmond/api/drf_api.py
+++ esmond/api/drf_api.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 from rest_framework_extensions.mixins import NestedViewSetMixin
+from rest_framework_extensions.fields import ResourceUriField
 
 from .models import *
 from esmond.util import atdecode, atencode
@@ -19,19 +20,6 @@ class OidsetViewset(viewsets.ModelViewSet):
     model = OIDSet
     serializer_class = OidsetSerializer
 
-class HyperlinkedRelatedEncodedField(relations.HyperlinkedIdentityField):
-    """
-    We need this for urls where the lookup_field names need to be 
-    atencoded.
-    """
-    def get_url(self, obj, view_name, request, format):
-        # Unsaved objects will not yet have a valid URL.
-        if hasattr(obj, 'pk') and obj.pk is None:
-            return None
-
-        lookup_value = getattr(obj, self.lookup_field)
-        kwargs = {self.lookup_url_kwarg: atencode(lookup_value)}
-        return self.reverse(view_name, kwargs=kwargs, request=request, format=format)
 
 class DecodeMixin(object):
     def get_object(self):
@@ -43,34 +31,83 @@ class DecodeMixin(object):
 
         return super(DecodeMixin, self).get_object()
 
+class EncodedHyperlinkField(relations.HyperlinkedIdentityField):
+    """
+    General url generator that handles atencoding the lookup_field.
+    """
+    def get_url(self, obj, view_name, request, format):
+        # Unsaved objects will not yet have a valid URL.
+        if hasattr(obj, 'pk') and obj.pk is None:
+            return None
+
+        lookup_value = getattr(obj, self.lookup_field)
+        kwargs = {self.lookup_url_kwarg: atencode(lookup_value)}
+        return self.reverse(view_name, kwargs=kwargs, request=request, format=format)
+
+
+class InterfaceHyperlinkField(relations.HyperlinkedIdentityField):
+    """
+    Generate urls to "fully qualified" interface detail.
+    """
+    def get_url(self, obj, view_name, request, format):
+        if hasattr(obj, 'pk') and obj.pk is None:
+            return None
+
+        lookup_value = getattr(obj, self.lookup_field)
+        kwargs = { 
+            'ifName': atencode(lookup_value),
+            'parent_lookup_device__name': obj.device.name,
+        }
+
+        view_name = 'device-interface-detail'
+
+        return reverse(view_name, kwargs=kwargs, request=request, format=format)
+
+
 class InterfaceSerializer(serializers.ModelSerializer):
-    serializer_url_field = HyperlinkedRelatedEncodedField
+    serializer_url_field = InterfaceHyperlinkField
     class Meta:
         model = IfRef
         fields = ('ifName','url')
         extra_kwargs={'url': {'lookup_field': 'ifName'}}
-        # lookup_field = 'ifName'
 
 class InterfaceViewset(DecodeMixin, viewsets.ModelViewSet):
     queryset = IfRef.objects.all()
     serializer_class = InterfaceSerializer
     lookup_field = 'ifName'
 
+
+
 class DeviceSerializer(DecodeMixin, serializers.ModelSerializer):
-    serializer_url_field = HyperlinkedRelatedEncodedField
+    serializer_url_field = EncodedHyperlinkField
     class Meta:
         model = Device
-        fields = ('name', 'url')
+        fields = ('url', 'name', 'ifref_set')
         extra_kwargs={'url': {'lookup_field': 'name'}}
-        # lookup_field = 'name'
+
+    def to_representation(self, obj):
+        self.fields['ifref_set'] = NestedInterfaceSerializer(required=False, many=True)
+        return super(DeviceSerializer, self).to_representation(obj)
 
 class DeviceViewset(viewsets.ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
     lookup_field = 'name'
 
+
+# Not sure if we need different resources for the nested interface resources,
+# but slip in these subclasses just in case.
+
+class NestedInterfaceSerializer(InterfaceSerializer):
+    pass
+
+class NestedInterfaceViewset(InterfaceViewset):
+    serializer_class = NestedInterfaceSerializer
+
+
+
 class DataSerializer(serializers.Serializer):
-    data = fields.DictField()
+    data = fields.DictField() 
 
 class DataViewset(viewsets.GenericViewSet):
     queryset = IfRef.objects.all()
@@ -78,6 +115,8 @@ class DataViewset(viewsets.GenericViewSet):
 
     def retrieve(self, request, **kwargs):
         print 'retrieve', kwargs
+        m = Device.objects.get(name='rtr_a')
+        print m, dir(m)
         d = dict(data=dict(ts=3, val='foo'))
         serializer = DataSerializer(d, context={'request': request})
         return Response(serializer.data)
--- esmond/urls.py
+++ esmond/urls.py
@@ -8,30 +8,46 @@ from esmond.api.api import v1_api
 from esmond.api.perfsonar.api import perfsonar_api
 
 from rest_framework import routers
+from rest_framework_extensions.routers import ExtendedSimpleRouter
+
 from esmond.api.drf_api import (
     DataViewset,
     DeviceViewset,
     InterfaceViewset,
+    NestedInterfaceViewset,
     OidsetViewset
 )
 
 router = routers.DefaultRouter()
 router.register('oidset', OidsetViewset)
-router.register('interface', InterfaceViewset,)
+router.register('interface', InterfaceViewset)
+# /device/ is registered as a nested resource below. need this
+# to make it show up in the DRF browsable API.
 router.register('device', DeviceViewset)
 
+extended_router = ExtendedSimpleRouter()
+
+device_router = extended_router.register(
+    r'device',
+    DeviceViewset,
+    base_name='device',
+)
+device_router.register(
+    r'interface',
+    NestedInterfaceViewset,
+    base_name='device-interface',
+    parents_query_lookups=['device__name']
+)
+
+# This object attribute has all of the patterns - helpful
+# when you need to look at the view names for reverse(), etc.
+# print extended_router.urls
+
 urlpatterns = patterns('',
-    # Example:
-    # (r'^ed/', include('ed.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'', include(v1_api.urls + perfsonar_api.urls)),
     (r'v2/',include(router.urls)),
-    (r'v2/interface/(?P<lookup>[^/]+)/(?P<type>[^/]+)$', DataViewset.as_view({'get': 'retrieve'})),
-    (r'v2/interface/(?P<lookup>[^/]+)/(?P<type>[^/]+)/(?P<subtype>.+)$', DataViewset.as_view({'get': 'retrieve'})),
+    (r'v2/',include(extended_router.urls)),
+    (r'v2/device/(?P<name>[^/]+)/interface/(?P<ifName>[^/]+)/(?P<type>[^/]+)$', DataViewset.as_view({'get': 'retrieve'})),
+    (r'v2/device/(?P<name>[^/]+)/interface/(?P<ifname>[^/]+)/(?P<type>[^/]+)/(?P<subtype>.+)$', DataViewset.as_view({'get': 'retrieve'})),
 )
