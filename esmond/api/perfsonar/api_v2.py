dd867d1d4ddef4434f565c7b1a6fee39df70ce4c
Monte Goode <MMGoode@lbl.gov>
2015-08-03 08:56:00 -0700

    Add in viewset stub methods.

--- esmond/api/perfsonar/api_v2.py
+++ esmond/api/perfsonar/api_v2.py
@@ -42,12 +42,35 @@ class ArchiveSerializer(serializers.ModelSerializer):
         model = PSMetadata
         fields = ('metadata_key', 'subject_type')
 
-class ArchiveViewset(viewsets.ModelViewSet):
+class ArchiveViewset(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                    viewsets.GenericViewSet):
+
+    """Implements GET and POST model operations w/specific mixins rather 
+    than using viewsets.ModelSerializer."""
+
     serializer_class = ArchiveSerializer
     lookup_field = 'metadata_key'
 
     def get_queryset(self):
         # Modify for custom filtering logic, etc
         ret = PSMetadata.objects.all()
-
         return ret
+
+    def list(self, request):
+        """Stub for list GET"""
+        return super(ArchiveViewset, self).list(request)
+
+    def retrieve(self, request, **kwargs):
+        """Stub for detail GET
+        'metadata_key', will be one of the kwargs since that is 
+        defined as the lookup field for the detail view"""
+        return super(ArchiveViewset, self).retrieve(request, **kwargs)
+
+    def create(self, request):
+        """Stub for POST object creation"""
+        return super(ArchiveViewset, self).create(request)
+
+
+
